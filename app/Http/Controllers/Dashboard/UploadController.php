fe12313206fd52002a7ed16419908609fa7ae106
vahid664 <vahiddarvishi918@gmail.com>
2023-12-09 14:19:45 +0330

    set storage for ckeditor uplod image

--- app/Http/Controllers/Dashboard/UploadController.php
+++ app/Http/Controllers/Dashboard/UploadController.php
@@ -4,21 +4,39 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 class UploadController extends Controller
 {
     public function upload(Request $request)
     {
+        // if ($request->hasFile('upload')) {
+        //     $originName = $request->file('upload')->getClientOriginalName();
+        //     $fileName = pathinfo($originName, PATHINFO_FILENAME);
+        //     $extension = $request->file('upload')->getClientOriginalExtension();
+        //     $fileName = $fileName . '_' . time() . '.' . $extension;
+        //     $request->upload->storeAs('public/media', $fileName);
+
+        //     // $request->file('upload')->move(public_path('media'), $fileName);
+
+        //     $url = asset('storage/media/' . $fileName);
+        //     return response()->json(['fileName' => $fileName, 'uploaded'=> 1, 'url' => $url]);
+        // }
+    
         if ($request->hasFile('upload')) {
-            $originName = $request->file('upload')->getClientOriginalName();
-            $fileName = pathinfo($originName, PATHINFO_FILENAME);
+            $originalName = $request->file('upload')->getClientOriginalName();
+            $fileName = Str::slug(pathinfo($originalName, PATHINFO_FILENAME));
             $extension = $request->file('upload')->getClientOriginalExtension();
-            $fileName = $fileName . '_' . time() . '.' . $extension;
+            $fileName = $fileName . "_" . time() . "." . $extension;
 
-            $request->file('upload')->move(public_path('media'), $fileName);
+            $request->file('upload')->storeAs('public/ckeditor_images', $fileName);
 
-            $url = asset('media/' . $fileName);
-            return response()->json(['fileName' => $fileName, 'uploaded'=> 1, 'url' => $url]);
+            $url = Storage::url('ckeditor_images/' . $fileName);
+
+            return response()->json(['uploaded' => true, 'url' => $url]);
         }
+
+        return response()->json(['uploaded' => false, 'error' => ['message' => 'Could not upload the image.']]);
+    
     }
 }
