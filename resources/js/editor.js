dcfb5990765ff7bcccb4dffc115017ad1b01f386
vahid664 <vahiddarvishi918@gmail.com>
2023-12-19 12:01:18 +0330

    modify editorjs to rtl

--- resources/js/editor.js
+++ resources/js/editor.js
@@ -11,7 +11,33 @@ const editor = new EditorJS({
 ment that should contain Editor instance
    */
   holder: 'editorjs',
-
+  i18n: {
+    /**
+     * Text direction
+     */
+    direction: 'rtl',
+    messages: {
+      toolNames: {
+        "Text": "متن",
+        "Heading": "سر تیتر",
+        "List": "لیست",
+        "Warning": "هشدار",
+        "Checklist": "چک لیست",
+        "Quote": "نقل قول",
+        "Code": "کد",
+        "Delimiter": "خط فاصله",
+        "Raw HTML": "درج HTML",
+        "Table": "جدول",
+        "Link": "لینک",
+        "Marker": "مارکر",
+        "Bold": "بولد",
+        "Italic": "آیتالیک",
+        "InlineCode": "کد در خط",
+        "Filter": "فیلتر",
+        "Image": "تصویر"
+      },
+    },
+},
 
   tools: {
     header: {
